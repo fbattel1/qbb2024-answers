0d6b5967d8c6d88cd4523b2d1cff189ec15c67f9
Francesca Battelli <fbattel1@jh.edu>
2024-09-26 14:19:07 -0400

    2.2 complete

---
+++ Week 2/QB_Week2_Exercise2.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+
+
+df <- read.table("~/Documents/Quant bio/qbb2024-answers/Week 2/snp_counts.txt", header = FALSE)
+headers <- c("MAF", "Feature", "Enrichment")
+colnames(df) <- headers
+
+#view(df)
+
+df2 <- df %>%
+  dplyr::mutate(Feature = gsub("_chr1.bed", "", Feature)) %>% # Mutate Feature column to just have feature name
+  dplyr::mutate(MAF = gsub("chr1_snps_", "", MAF)) %>%
+  dplyr::mutate(MAF = gsub(".bed", "", MAF))
+
+#view(df2)
+
+
+ggplot(data = df2, aes(x = MAF, y = Enrichment, color = Feature, group = Feature)) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(trans = "log2") +
+  ggtitle("MAF Enrichment by Feature")
+ggsave(filename = "snp_enrichments.pdf")
+
+
+
+
+
+  
+  
