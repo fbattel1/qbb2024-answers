bd3442aac814541d82646d2295cd539d9ba1c589
Francesca Battelli <fbattel1@jh.edu>
2024-10-02 15:25:56 -0400

    unfinished

---
+++ Week 3/QB_Week3_Exercise1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Working with Illumina short-read seq
+# Study: lab strain of S. cervisciae crossed with wine strain to map associations between genotype and phenotype
+# Diploid offspring sporulated, colony from haploid spore sequenced 
+# Data: yeast sergegants, or haploid genomes that should be mosaic of OG strains
+# Mosaic = having long tracks of ancestry from one strain instead of another 
+
+# Load seq data into library and unzip 
+
+# Exercise 1
+# Quality control. Only argument it takes is fastq. Produces HTML as output
+# Open HTML in finder and will open webpage 
+
+# Q1.1 
+wc -l A01_09.fastq
+
